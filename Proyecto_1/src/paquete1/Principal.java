4da049b2391979598d0423c77e2d2191904bf66a
DAM124 <DAM124@C0115.iesataulfo.local>
2023-03-22 17:26:49 +0100

    Tercera modificación.

--- Proyecto_1/src/paquete1/Principal.java
+++ Proyecto_1/src/paquete1/Principal.java
@@ -3,7 +3,7 @@ package paquete1;
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("Programa que nos permitirá hacer un control de versiones con Git"+" Segunda modificacion");
+		System.out.println("Programa que nos permitirá hacer un control de versiones con Git"+" Segunda modificacion"+ "Tercera modificacion");
 
 	}
 
